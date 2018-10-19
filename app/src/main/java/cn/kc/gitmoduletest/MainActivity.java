9da2101b8e555f49c88f5ffc17eac1b3a3ca0e50
zhangzhuojia <zhangzhuojia2167@dingtalk.com>
2018-10-19 14:10:59 +0800

    add toast

--- app/src/main/java/cn/kc/gitmoduletest/MainActivity.java
+++ app/src/main/java/cn/kc/gitmoduletest/MainActivity.java
@@ -2,12 +2,19 @@ package cn.kc.gitmoduletest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import cn.kc.mylibrary.IndexView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
