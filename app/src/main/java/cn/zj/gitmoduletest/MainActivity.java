9ea1f89814190b7145dcec8b0e1ea4dc708034b4
zhangzhuojia <zhangzhuojia2167@dingtalk.com>
2018-10-19 16:28:44 +0800

    delete branch

--- app/src/main/java/cn/zj/gitmoduletest/MainActivity.java
+++ app/src/main/java/cn/zj/gitmoduletest/MainActivity.java
@@ -3,6 +3,8 @@ package cn.zj.gitmoduletest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.kc.gitmoduletest.R;
+
 public class MainActivity extends AppCompatActivity {
 
 
